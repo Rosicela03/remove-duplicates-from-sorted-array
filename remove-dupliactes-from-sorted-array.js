63e0b1bab0509030f7ee71ab34bd3e5ae9d29813
Rosicela03 <rosicelaluna03@gmail.com>
2025-10-02 16:38:54 -0400

    Update remove-dupliactes-from-sorted-array.js

--- remove-dupliactes-from-sorted-array.js
+++ remove-dupliactes-from-sorted-array.js
@@ -3,13 +3,29 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    let index = 0; 
+
+//keeps track of where each unique number will be written it starts at 0 because thats where we write our first number 
+    let indexCounter = 0; 
+
+    //this will run until we've gone through all elements in nums 
     for (let i=0; i < nums.length; i++){
+
+        // checking to see if our current element is different from the previous one if it is then this is a unique element
+        // for nums[0] its a different case since is always unique since nums[-1] will be undefined 
         if(nums[i] != nums[i-1]){
-            nums[index] = nums[i]
-            index++; 
+            
+            // since they are different the current element of nums will be placed on the position pointed by indexCounter
+            nums[indexCounter] = nums[i]
+            
+            //we need to increase positon of indexCounter so the following unique element is placed in a new positon 
+            indexCounter++; 
         }
     }
-    return index;
+
+    //now we're returning the count all the unique elements 
+    return indexCounter;
     
 };
+
+    
+
